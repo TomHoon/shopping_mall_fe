d97b27d2cb13ee263e4786a50dd13593c45a533b
Lee-HaeSung <haesung116@naver.com>
2026-03-02 15:50:24 +0900

    feat: 메인페이지 작업중인데까지

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,10 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import prettierConfig from 'eslint-config-prettier';
-import prettierPlugin from 'eslint-plugin-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-
-  {
-    plugins: {
-      prettier: prettierPlugin,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  },
 
   prettierConfig,
 
